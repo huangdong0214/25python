ff65755c8400e3609ee388c4bdd35e46a2fbe6bd
huang dong <huangdong0214@gmail.com>
2026-03-09 19:28:53 +0800

    首次提交

---
+++ 一个模块用到另一个模块/model1.py
@@ -0,0 +1,9 @@
+i = 10
+
+
+def print_line(char, times):
+    print(char * times)
+
+
+if __name__ == '__main__':  # 其他模块要导入model1模块时，if __name__ == '__main__':下面的代码不会被执行
+    pass
---
+++ 一个模块用到另一个模块/在本模块中使用model1.py
@@ -0,0 +1,7 @@
+import model1
+
+model1.print_line('*', 50)
+
+print(model1.i)
+
+print(model1.__name__)
